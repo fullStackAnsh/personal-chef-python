2d02d18b6a571d9d7f26f2b855952542ea95d278
fullStackAnsh <anshkanojia13@gmail.com>
2026-03-29 12:32:05 +0530

    Implemented the incredient identification logic , Conversion of image to base64 , multimodal llm call.

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from dotenv import load_dotenv
 import os
 from PIL import Image
+import base64
+from io import BytesIO
 from langchain_google_genai import ChatGoogleGenerativeAI
+import json
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 model = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",   
+    model="gemini-2.5-flash",
     temperature=0.3
 )
 
-response = model.invoke("Suggest 2 Indian Surnames ")
+def pil_to_base64(image: Image.Image):
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    return base64.b64encode(buffered.getvalue()).decode()
 
-print(response.content)
+image = Image.open(r"D:\desktop_backup\PortfolioUI\portfolio\public\fridge.jpg")
+image_base64 = pil_to_base64(image)
 
+message = [ {"role": "user",
+             "content": [
+                 {
+                     "type": "text",
+                     "text": """You are a JSON API. Extract all visible food ingredients. Return ONLY valid JSON. Do NOT include markdown. Do NOT include explanation.
+    Format:{"ingredients": ["item1", "item2"]}"""},
+            {
+                "type": "image_url",
+                "image_url": f"data:image/jpeg;base64,{image_base64}"
+            }
+        ]
+    }
+]
+
+response = model.invoke(message)
+data = json.loads(response.content)
+ingredients = data["ingredients"]
